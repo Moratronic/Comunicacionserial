5daad081a93c581643c017e5c711682ba98d5827
Moratronic <angel.moraparr@gmail.com>
2021-11-23 02:54:44 -0600

    Sin timer

--- Comunicacionserial/Form1.cs
+++ Comunicacionserial/Form1.cs
@@ -22,14 +22,15 @@ namespace Comunicacionserial
         Boolean temblor=false;
 
         string portselec;
-        static int N= 256;//Tamaño de la muestra de 256
+        static int N= 32;//Tamaño de la muestra de 32
 
         static int Fs = 25; //Frecuencia de muestreo
         static int m = 25;    //Numero de muestras
-        Complex[] x = new Complex[N];
-        Complex[] x0 = new Complex[m+1];
+        Complex[] x = new Complex[m];
+        Complex[] x0 = new Complex[m + 1];
+        Complex[] X = new Complex[N];
         //Señal de prueba nadamas
-        //static readonly double[] entrada = Generate.Sinusoidal(m, 25, 6, 2);
+        static readonly double[] entrada = Generate.Sinusoidal(m, 25, 6, 2);
         double[] magX = new double[N /2];
 
         public Form1()
@@ -52,54 +53,59 @@ namespace Comunicacionserial
 
             chart2.ChartAreas[0].AxisY.Title = "dB";
             chart2.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 13.0f);
-            //Grafico la señal  que cree
-           /* for (int n = 0; n < m; n++)
-            {
-                double tiempo = n *1.0/ Fs;
-                chart1.Series[0].Points.AddXY(tiempo, entrada[n]);
-            }
-            //Arreglo que guarda los datos
-            //Se rellena de los valores medidos
+            //Se rellenan ceros de x
             for (int i = 0; i <m; i++)
             {
-                x[i] = new Complex(entrada[i], 0);
+                x[i] = 0;
             }
+            //Grafico la señal  que cree
+             for (int n = 0; n < m; n++)
+             {
+                 double tiempo = n *1.0/ Fs;
+                 chart1.Series[0].Points.AddXY(tiempo, entrada[n]);
+             }
+             //Arreglo que guarda los datos
+             //Se rellena de los valores medidos
+             for (int i = 0; i <m; i++)
+             {
+                 x[i] = new Complex(entrada[i], 0);
+             }
 
-            PlotFFT();*/
+             PlotFFT();
+  
         }
 
         public void PlotFFT()
         {
-            //Se aplica ventana Hanning
+            chart2.Series[0].Points.Clear();
+
+            //Se aplica ventana Hanning 
             for (int i = 0; i < m; i++)
             {
-                x[i] = x[i] * (0.5 - (0.5 * Math.Cos((2 * Math.PI * i) / m - 1)));
+                X[i] = x[i];
+                X[i] = X[i] * (0.5 - (0.5 * Math.Cos((2 * Math.PI * i) / m - 1)));
             }
-
             //Se aplica Zero padding
             for (int i = m; i < N; i++)
             {
-                x[i] = new Complex(0, 0);
+                X[i] = 0;
             }
-
-            chart2.Series[0].Points.Clear();
-
+  
             //Forward Fourier convierte el tiempo en frecuencia
-            Fourier.Forward(x, FourierOptions.NoScaling); //No se se escalan los resultados
-
+            Fourier.Forward(X, FourierOptions.NoScaling); //No se se escalan los resultados
             //Se toma solo unas muestra del arreglo ya transformado
             for (int i = 0; i < (N/2); i++)
             {
                 //Se obtiene la magnitud de la Transformada de Fourier =abs[sqrt(r^2+i^2)]
-                magX [i]= Math.Abs(Math.Sqrt(Math.Pow(x[i].Real, 2) + Math.Pow(x[i].Imaginary, 2)));
-                if(magX[i]==0)
+                magX [i]= Math.Abs(Math.Sqrt(Math.Pow(X[i].Real, 2) + Math.Pow(X[i].Imaginary, 2)));
+
+                if (magX[i]==0)
                 {
                     magX[0] = 0.001;  
                 }
-                double dB = 10*Math.Log10(magX[i]);
-
+                double dB = 20*Math.Log10(magX[i]);
                 //Determinación de Hz 
-                double Hz = (i * Fs/ 256.0);
+                double Hz = (i * Fs*1.0/ N);
                 Hz = Math.Round(Hz, 2);
                 chart2.Series[0].Points.AddXY(Hz, dB); //Multiplicar i *fmuestreo
             }
@@ -156,7 +162,6 @@ namespace Comunicacionserial
         }
 
         Boolean i = false;
-        int c = 0;
         public void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)//Recepcion de datos
         {
             //inicia el timer1 para que sea cero cuando se recibe el primer dato
@@ -167,35 +172,32 @@ namespace Comunicacionserial
                 bool input = double.TryParse(serialPort1.ReadLine(), out numero);
                 if (!input)
                  return;
-
                 if (i == false)
                 {
                     t = 0;
                     i = true;
                 }
-
                 double tap = Math.Round(((numero * 4.0) / 65535), 2);
                 chart1.Series[0].Points.AddXY(t, tap);
                 label4.Text = tap.ToString();
 
                 //Arreglo que guarda los datos
                 //Se rellena de los valores medidos
-                if (c<= m)//Si c es menor o igual a 25 
-                {
-                    x0[c] = new Complex(tap, 0);  //Se rellena la posicion desde 0 a 25 hasta que c ==26
-                    c ++;
-                }
-                if (c>m) //Si c>25
-                {
-                    c = 25;
+
+                x0[25] = tap;  //Se rellena la posicion 25      
                     for (int i = 0; i < m; i++) //Recorrimiento desde 0 hasta 24 tomando valor de 25
                     {
-                        x[i] = x0[i+1];
-                        x0[i] = x[i];
-                        x0[i + 1] = 0;
+                        x0[i] = x0[i+1];
+                        x[i] = x0[i];
                     }
-                    g = 1;
+                g ++;
+                if (temblor == true&& g>=1)
+                {
+                    g = 0;
+                    PlotFFT();
+                     
                 }
+
             }
         } 
         
@@ -210,12 +212,12 @@ namespace Comunicacionserial
         private void timer1_Tick(object sender, EventArgs e)
         {
             t = t+0.04;
-            if (inicio == true && temblor==true && g==1)
+            /*if (inicio == true && temblor==true && g>=1)
             {
                PlotFFT();
                 g = 0;
-            }
-            if (t>=20.0)
+            }*/
+            if (inicio ==true && t>=20.0)
             {
                 inicio = false;
                 serialPort1.Write("S");
@@ -292,7 +294,7 @@ namespace Comunicacionserial
 
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)//Seleccion de tipo de prueba
         {
             if (comboBox2.Text == "Prueba TAP")
             {
@@ -304,6 +306,6 @@ namespace Comunicacionserial
             {
                 temblor = true;
             }
-        }//Seleccion de tipo de prueba
+        }
     }
 }
